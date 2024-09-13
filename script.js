7484e4e9b5b6b2ccd0640468ee092d9e1359d327
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-12 23:08:23 -0400

    created 3 more variables to access the html elment

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ The HTML and CSS of this project have been provided for you, so you can focus on
 Start by accessing the #playlist-songs, #play, and #pause elements with the getElementById() method.
 Assign them to variables playlistSongs, playButton and pauseButton respectively.
 
+
+**Step 2
+Access the #next, #previous and #shuffle elements from the HTML file.
+Assign them to variables named nextButton, previousButton, and shuffleButton, respectively.
 */
 
-const playlistSongs = document.getElementById('playlist-songs');
-const playButton = document.getElementById('play');
-const pauseButton = document.getElementById('pause');
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
