69461b85c8eb808df79dd7d52fe3c354fe8ebfc6
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-13 22:42:23 -0400

    created an array named allSongs and its first value

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ Assign them to variables playlistSongs, playButton and pauseButton respectively.
 **Step 2
 Access the #next, #previous and #shuffle elements from the HTML file.
 Assign them to variables named nextButton, previousButton, and shuffleButton, respectively.
+
+
+**Step 3
+Next, create an array to store all the songs.
+Create an empty allSongs array.
+
+
+**Step 4
+Inside the allSongs array, create an object with the following properties and values:
+Example Code
+id: 0,
+title: "Scratching The Surface",
+artist: "Quincy Larson",
+duration: "4:25",
+src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
 */
 
 const playlistSongs = document.getElementById("playlist-songs");
@@ -18,3 +33,12 @@ const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
+const allSongs = [
+    {
+        id: 0,
+        title: "Scratching The Surface",
+        artist: "Quincy Larson",
+        duration: "4:25",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+    },
+];
