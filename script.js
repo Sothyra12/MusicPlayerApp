e8e70591c28e309ca7c8c1c1d5f89e3405550e64
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-19 23:43:24 -0400

    used the implicit return without curly braces in the function

--- script.js
+++ script.js
@@ -112,6 +112,25 @@ Inside that console statement, add the template literal ${org} is awesome!.
 Below your printMessage function, call the function and pass in the string "freeCodeCamp"
 as an argument.
 Open up the console to see the result.
+
+
+**Step 15
+If the arrow function is returning a simple expression,
+you can omit the return keyword and the curly braces {}.
+This is called an implicit return.
+Example Code
+const multiplyTwoNumbers = (num1, num2) => num1 * num2;
+If your arrow function has multiple lines of code in the function body,
+then you need to use the return keyword and the curly braces {}.
+Example Code
+const getTax = (price) => {
+  const taxRate = 0.08;
+  const tax = price * taxRate;
+  return tax;
+};
+Refactor, or update, your addTwoNumbers function to remove the return keyword and the curly braces {}.
+Your addTwoNumbers function should instead use an implicit return.
+Open up the console to make sure that you are still getting the correct output.
 */
 
 const playlistSongs = document.getElementById("playlist-songs");
@@ -161,3 +180,13 @@ const printMessage = (org) => {
     console.log(`${org} is awesome!`);
   }
 printMessage("freeCodeCamp");
+
+
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+  }
+
+console.log(addTwoNumbers(3,4));
+
+const addTwoNumber = (num1, num2) => num1 + num2;
+console.log(addTwoNumber(5,6));
