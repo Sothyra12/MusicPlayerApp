b7bcc727def85951139753547cd0c9b5bc7121d9
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-24 23:46:40 -0400

    used optional chaining(?.)

--- script.js
+++ script.js
@@ -205,6 +205,31 @@ array.map().join();
 Next, you will need to update the playlist in your HTML document to display the songs.
 Assign songsHTML to the innerHTML property of the playlistSongs element.
 This will insert the li element you just created into the ul element in the already providedHTML file.
+
+
+**Step 26
+Now you need to call the renderSongs function and pass in userData?.
+songs in order to finally display the songs in the UI.
+Optional chaining (?.) helps prevent errors when accessing
+nested properties that might be null or undefined. For example:
+
+Example Code
+const user = {
+  name: "Quincy",
+  address: {
+    city: "San Francisco",
+    state: "CA",
+    country: "USA",
+  },
+};
+
+// Accessing nested properties without optional chaining
+const state = user.address.state; // CA
+
+// Accessing a non-existent nested property with optional chaining
+const zipCode = user.address?.zipCode; // Returns undefined instead of throwing an error
+Call the renderSongs function with the songs property of userData.
+This will render the songs in the playlist.
 */
 
 const playlistSongs = document.getElementById("playlist-songs");
@@ -262,7 +287,7 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 }
 
-
+renderSongs(userData?.songs);
 
 // const printGreeting = () => {
 //     console.log("Hello there!");
