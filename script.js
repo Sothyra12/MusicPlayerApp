20acb9d518a750649c155b04c3cf4492b7b79d53
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-27 22:06:20 -0400

    added a callback function with a & b paraters to the sorting method

--- script.js
+++ script.js
@@ -243,6 +243,31 @@ Example Code
 const names = ["Tom", "Jessica", "Quincy", "Naomi"];
 names.sort() // ["Jessica", "Naomi", "Quincy", "Tom"]
 
+
+**Step 29
+To sort the songs in alphabetical order by title, you will need to pass in a
+compare callback function into your sort() method.
+Here is an example of sorting a list of fruits by name.
+Example Code
+const fruits = [
+  { name: "Apples", price: 0.99 },
+  { name: "Blueberries", price: 1.49 },
+  { name: "Grapes", price: 2.99 },
+];
+
+fruits.sort((a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+
+  return 0;
+});
+In the next few steps, you will learn what each of those if statements is
+doing inside that callback function. But for now, add an empty callback function to your sort()
+method and use a and b for the parameter names.
 */
 
 const playlistSongs = document.getElementById("playlist-songs");
@@ -303,7 +328,7 @@ const renderSongs = (array) => {
 renderSongs(userData?.songs);
 
 const sortSongs = () => {
-  userData?.songs.sort();
+  userData?.songs.sort((a,b)=> {});
 };
 
 // const printGreeting = () => {
