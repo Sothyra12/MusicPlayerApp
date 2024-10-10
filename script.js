567e5b7ef19eb82b299c90339257c1dc11c8cdf6
Sothyra12 <schan256@my.centennialcollege.ca>
2024-10-09 23:18:29 -0400

    created a highlightCurrentSong()

--- script.js
+++ script.js
@@ -481,6 +481,16 @@ const playPreviousSong = () => {
 };
 
 previousButton.addEventListener("click", playPreviousSong);
+
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+};
 // const printGreeting = () => {
 //     console.log("Hello there!");
 //   };
