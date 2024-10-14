031fdcfed729f975907b0edbadd228e2a7f2c76a
Sothyra12 <schan256@my.centennialcollege.ca>
2024-10-13 23:52:31 -0400

    created another if statement to check if the playlist is empty

--- script.js
+++ script.js
@@ -156,6 +156,25 @@ const deleteSong = (id) => {
   renderSongs(userData?.songs);
   highlightCurrentSong();
   setPlayButtonAccessibleText();
+
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(sortSongs());
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+  }
+
 };
 
 const setPlayerDisplay = () => {
